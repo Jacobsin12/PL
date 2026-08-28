f5315f3f62aa30a38891f7e74640da59974b023f
Admin <admin@example.com>
2026-08-28 13:39:20 -0600

    Primera versión del Dashboard

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'sd-dash-v6';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return Promise.all(
+          urlsToCache.map(url => {
+            return cache.add(new Request(url, { cache: 'reload' }));
+          })
+        );
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
